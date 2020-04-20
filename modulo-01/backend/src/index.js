d9e928f905513ec9e5aad7e0d0fd1586e43284bd
drd-2rcl <drd.rclrd@gmail.com>
2020-04-20 10:08:15 -0300

    Add new routes and params

--- modulo-01/backend/src/index.js
+++ modulo-01/backend/src/index.js
@@ -2,8 +2,35 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({ message: "hello" });
+app.use(express.json());
+
+app.get("/projects", (req, res) => {
+  // query params
+  const { title, owner } = req.query;
+
+  console.log("title", title);
+  console.log("owner", owner);
+
+  return res.json(["Projeto 1", "Projeto 2"]);
+});
+
+app.post("/projects", (req, res) => {
+  const { title, owner } = req.body;
+  console.log("title", title);
+  console.log("owner", owner);
+  return res.json(["Projeto 1", "Projeto 2", "Projeto 3"]);
+});
+
+app.put("/projects/:id", (req, res) => {
+  // route params
+  const { id } = req.params;
+  console.log("id", id);
+
+  return res.json(["Projeto 4", "Projeto 2", "Projeto 3"]);
+});
+
+app.delete("/projects/:id", (req, res) => {
+  return res.json(["Projeto 2", "Projeto 3"]);
 });
 
 app.listen(3333, () => {
