9d30222bf7e11bac050fa8b7d636996e03dbd084
drd-2rcl <drd.rclrd@gmail.com>
2020-04-19 21:56:49 -0300

    Add message when server is UP

--- modulo-01/backend/src/index.js
+++ modulo-01/backend/src/index.js
@@ -3,7 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  return res.json({ message: "hi" });
+  return res.json({ message: "hello" });
 });
 
-app.listen(3333);
+app.listen(3333, () => {
+  console.log("✔ Server UP!");
+});
