fbd2df86fa258fef44738edb942d720576ae00bb
drd-2rcl <drd.rclrd@gmail.com>
2020-04-21 20:06:47 -0300

    Create middleware to log request and validate id

--- modulo-01/backend/src/index.js
+++ modulo-01/backend/src/index.js
@@ -1,10 +1,35 @@
 const express = require("express");
-const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 const app = express();
 
 app.use(express.json());
 
 const projects = [];
+
+function logRequest(req, res, next) {
+  const { method, url } = req;
+
+  const logLabel = `[${method}] ${url}`;
+
+  console.time(logLabel);
+
+  next();
+
+  console.timeEnd(logLabel);
+}
+
+function validateProjectId(req, res, next) {
+  const { id } = req.params;
+
+  if (!isUuid(id)) {
+    return res.status(400).json({ error: "Invalid project ID." });
+  }
+
+  return next();
+}
+
+app.use(logRequest);
+app.use("/projects/:id", validateProjectId);
 
 app.get("/projects", (req, res) => {
   return res.json(projects);
