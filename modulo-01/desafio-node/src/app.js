31c639b69ac347b3e20248ea198d2e3d12ed5430
drd-2rcl <drd.rclrd@gmail.com>
2020-04-22 08:59:04 -0300

    Add route to create a repository and pass in test

--- modulo-01/desafio-node/src/app.js
+++ modulo-01/desafio-node/src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -15,7 +15,19 @@ app.get("/repositories", (request, response) => {
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
