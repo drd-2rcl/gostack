98e1a20a856a6ecaf19205fc5fa3aeb185b47ee2
drd-2rcl <drd.rclrd@gmail.com>
2020-04-24 10:01:13 -0300

    Property and children concepts

--- modulo-01/frontend/src/App.js
+++ modulo-01/frontend/src/App.js
@@ -2,7 +2,23 @@ import React from "react";
 import Header from "./components/Header";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header title="Homepage">
+        <ul>
+          <li>Homepage</li>
+          <li>Project</li>
+        </ul>
+      </Header>
+      <Header title="Projects">
+        <ul>
+          <li>Homepage</li>
+          <li>Project</li>
+          <li>Login</li>
+        </ul>
+      </Header>
+    </>
+  );
 }
 
 export default App;
