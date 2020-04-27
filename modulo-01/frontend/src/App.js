935e416d70a2c342650e8791d91739be66a9dc4d
drd-2rcl <drd.rclrd@gmail.com>
2020-04-26 22:22:21 -0300

    Add new image and use in initial page

--- modulo-01/frontend/src/App.js
+++ modulo-01/frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Header from "./components/Header";
 import "./App.css";
+import backgroundImage from "./assets/background.jpeg";
 
 function App() {
   const [projects, setProjects] = useState([
@@ -14,6 +15,7 @@ function App() {
   return (
     <>
       <Header title="Projects" />
+      <img width={300} src={backgroundImage} />
       <ul>
         {projects.map((project) => (
           <li key={project}>{project}</li>
