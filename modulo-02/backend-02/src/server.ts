ba0ca649b095796e027c63837effff3e91cc2e6c
drd-2rcl <drd.rclrd@gmail.com>
2020-07-01 23:06:42 -0300

    Install and fix

--- modulo-02/backend-02/src/server.ts
+++ modulo-02/backend-02/src/server.ts
@@ -1,11 +1,11 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "hi" });
+app.get('/', (request, response) => {
+  return response.json({ message: 'hi' });
 });
 
 app.listen(3333, () => {
-  console.log("Server started on port 3333");
+  console.log('Server started on port 3333');
 });
