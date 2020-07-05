e0b105b400585203fce4e00fb96ba0106dc33dd0
drd-2rcl <drd.rclrd@gmail.com>
2020-07-05 20:55:01 -0300

    Add date-fns to format date

--- modulo-02/backend-02/src/routes/appointments.routes.ts
+++ modulo-02/backend-02/src/routes/appointments.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO } from 'date-fns';
 
 const appointmentsRouter = Router();
 
@@ -8,10 +9,12 @@ const appointments = [];
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
+  const parsedDate = startOfHour(parseISO(date));
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   };
 
   appointments.push(appointment);
