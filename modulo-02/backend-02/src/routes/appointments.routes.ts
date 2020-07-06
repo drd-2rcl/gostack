431efa51e534ce9fc1763649ce42b5ee2c436ec7
drd-2rcl <drd.rclrd@gmail.com>
2020-07-05 21:17:38 -0300

    Add type to appointment

--- modulo-02/backend-02/src/routes/appointments.routes.ts
+++ modulo-02/backend-02/src/routes/appointments.routes.ts
@@ -4,7 +4,13 @@ import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
