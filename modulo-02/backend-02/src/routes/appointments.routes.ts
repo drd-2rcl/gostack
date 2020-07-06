9cd8875827587765b21f64d4cfae632926bebf4a
drd-2rcl <drd.rclrd@gmail.com>
2020-07-05 21:16:21 -0300

    Add method to find appointments and conditional

--- modulo-02/backend-02/src/routes/appointments.routes.ts
+++ modulo-02/backend-02/src/routes/appointments.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
-import { startOfHour, parseISO } from 'date-fns';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
@@ -10,6 +10,16 @@ appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
   const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
 
   const appointment = {
     id: uuid(),
