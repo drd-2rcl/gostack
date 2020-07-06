41c3a7c5428a194092dfa0a37c9bbb1c87b81cfe
drd-2rcl <drd.rclrd@gmail.com>
2020-07-05 21:44:35 -0300

    Refactoring, move methods to controller

--- modulo-02/backend-02/src/routes/appointments.routes.ts
+++ modulo-02/backend-02/src/routes/appointments.routes.ts
@@ -1,18 +1,18 @@
 import { Router } from 'express';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
-import Appointment from '../models/Appointment';
+import { startOfHour, parseISO } from 'date-fns';
+
+import AppointmentsController from '../controllers/AppointmentsController';
 
 const appointmentsRouter = Router();
-
-const appointments: Appointment[] = [];
+const appointmentsController = new AppointmentsController();
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
   const parsedDate = startOfHour(parseISO(date));
 
-  const findAppointmentInSameDate = appointments.find(appointment =>
-    isEqual(parsedDate, appointment.date),
+  const findAppointmentInSameDate = appointmentsController.findByDate(
+    parsedDate,
   );
 
   if (findAppointmentInSameDate) {
@@ -21,9 +21,7 @@ appointmentsRouter.post('/', (request, response) => {
       .json({ message: 'This appointment is already booked' });
   }
 
-  const appointment = new Appointment(provider, parsedDate);
-
-  appointments.push(appointment);
+  const appointment = appointmentsController.create(provider, parsedDate);
 
   return response.json(appointment);
 });
