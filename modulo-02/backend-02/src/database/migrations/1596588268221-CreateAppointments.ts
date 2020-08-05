6c94cd775a04cf237926a7ad1cdb965974f0fd86
drd-2rcl <drd.rclrd@gmail.com>
2020-08-04 21:46:46 -0300

    Create first migration to create appointments

---
+++ modulo-02/backend-02/src/database/migrations/1596588268221-CreateAppointments.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateAppointments1596588268221 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
