5b13bd05b410c9f7432dd98486ad058589bdde84
drd-2rcl <drd.rclrd@gmail.com>
2020-08-06 10:04:01 -0300

    Add propertie default with db postgres

--- modulo-02/backend-02/src/database/migrations/1596588268221-CreateAppointments.ts
+++ modulo-02/backend-02/src/database/migrations/1596588268221-CreateAppointments.ts
@@ -12,6 +12,7 @@ export class CreateAppointments1596588268221 implements MigrationInterface {
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()'
           },
           {
             name: 'provider',
